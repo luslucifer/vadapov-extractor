4b7ac968914974d698fab3df7ac4e0da7b9db500
luslucifer <100895428+luslucifer@users.noreply.github.com>
2024-05-31 22:45:37 +0600

    tv added

--- main.py
+++ main.py
@@ -2,30 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import json
-from flask import Flask
-import os , sys
-
+from flask import Flask, request, jsonify
+import os
+from urllib.parse import quote
 
 class VadaPov:
     def __init__(self):
         self.ROOT = 'https://vadapav.mov'
         self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
         self.headers = {'User-Agent': self.user_agent}
-        self.tmdb_api_key = 'f6f2a9e9b0f5eed81b4cabe35d5a9c1b'  # Move API key to a variable
+        self.tmdb_api_key = 'f6f2a9e9b0f5eed81b4cabe35d5a9c1b'
         self.MvStringPattern = r'\d{3,4}p'
+        self.types = ['movie', 'tv']
 
-
-    def getTmdbTitle(self, id: str | int):
-        url = f'https://api.themoviedb.org/3/movie/{id}?append_to_response=videos&api_key={self.tmdb_api_key}'
+    def getTmdbTitle(self, id, type_=0):
+        t = ['title', 'name']
+        url = f'https://api.themoviedb.org/3/{self.types[type_]}/{id}?append_to_response=videos&api_key={self.tmdb_api_key}'
         res = requests.get(url).json()
-        title = res['title'].replace(':','')
-        release_year = res['release_date'][:4]
-        ul_title = f'{title} ({release_year})'
-        return title, ul_title
+        title = res[t[type_]].replace(':', '')
+        release_year = ''
+        if type_ != 1:
+            release_year = f" ({res['release_date'][:4]})"
+        ul_title = f'{title}{release_year}'
+        return ul_title
 
-    def get_links(self, query: str):
+    def get_links(self, query):
         arr = []
-        res = requests.get(url=f'{self.ROOT}/s/{query}', headers=self.headers)
+        res = requests.get(url=f'{self.ROOT}/s/{quote(query)}', headers=self.headers)
         soup = BeautifulSoup(res.content, 'html.parser')
         directory_div = soup.find('div', class_='directory')
         links = directory_div.find_all('a')
@@ -33,28 +36,31 @@ class VadaPov:
             part = link.get('href')
             arr.append(f'{self.ROOT}{part}')
         return arr
-    def extract_file_details (self,li:BeautifulSoup): 
+
+    def extract_file_details(self, li, ss=None, ep=None):
         try:
             a = li.find('a')
-            size = li.find('div',class_='size-div').get_text()
+            size = li.find('div', class_='size-div').get_text()
             partial_link = a.get('href')
             link = f'{self.ROOT}{partial_link}'
             name = a.get_text()
             quality = None
-            match = re.search(self.MvStringPattern,name)
-            if match : 
+            match = re.search(self.MvStringPattern, name)
+            if match:
                 quality = match.group()
-
             type_ = name.split('.')[-1]
-            # print(a.get_text())
-            return {'name':name,'downloadLink':link,'size':size,'quality':quality,'type':type_}
-        
-        except Exception as err : 
+            default_obj = {'name': name, 'downloadLink': link, 'size': size, 'quality': quality, 'type_': type_}
+
+            if ss and ep:
+                default_obj['season'] = ss
+                default_obj['episode'] = ep
+            return default_obj
+
+        except Exception as err:
             print(err)
-            # return err
+            return None
 
-
-    def process_link(self, link: str):
+    def process_link(self, link):
         res = requests.get(link, headers=self.headers)
         soup = BeautifulSoup(res.content, 'html.parser')
         directory_div = soup.find('div', class_='directory')
@@ -65,35 +71,71 @@ class VadaPov:
             if obj:
                 arr.append(obj)
         return arr
-    
-    def main(self, id: str | int):
-        title, ul_title = self.getTmdbTitle(id)
-        links = self.get_links(ul_title)
-        arr = []
-        if links:
-            for link in links :
-                ar = self.process_link(link)
-                arr = arr+ar
-        return arr
-    
+
+    def tv_season(self, link, no):
+        res = requests.get(link, headers=self.headers)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        directory_div = soup.find('div', class_='directory')
+        all_a = directory_div.find_all('a')
+        for a in all_a:
+            try:
+                season_no = a.get_text()[-2:]
+                if int(season_no) == int(no):
+                    p_link = a.get('href')
+                    link = f'{self.ROOT}{p_link}'
+                    return link
+            except Exception as err:
+                print(err)
+        return None
+
+    def tv_download(self, link, ss, ep):
+        res = requests.get(link, headers=self.headers)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        directory_div = soup.find('div', class_='directory')
+        lis = directory_div.find_all('li')
+        pattern = rf'S0?{ss}E0?{ep}\.'
+        for li in lis:
+            try:
+                a = li.find('a')
+                match_ = re.search(pattern, a.get_text())
+                if match_:
+                    return self.extract_file_details(li=li, ss=ss, ep=ep)
+            except Exception as err:
+                print(err)
+        return None
+
+    def main(self, id, ss=None, ep=None):
+        if ss and ep:
+            ul_title = self.getTmdbTitle(id, 1)
+            links = self.get_links(ul_title)
+            link = self.tv_season(links[0], ss)
+            return [self.tv_download(link, ss, ep)]
+        else:
+            ul_title = self.getTmdbTitle(id, 0)
+            links = self.get_links(ul_title)
+            arr = []
+            if links:
+                for link in links:
+                    ar = self.process_link(link)
+                    arr.extend(ar)
+            return arr
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return ' made in chaina '
-
+    return 'Made in China'
 
 @app.route('/<id>')
-def Movie(id):
-    try :  
+def media(id):
+    ss = request.args.get('ss')
+    ep = request.args.get('ep')
+    try:
         v = VadaPov()
-        x = v.main(id)
-        return x
-    except Exception as err : 
-        return {'message':'movie not found '}
+        x = v.main(id, ss, ep)
+        return jsonify(x)
+    except Exception as err:
+        return jsonify({'message': str(err)}), 500
+
 if __name__ == "__main__":
-    port = os.getenv("PORT", "8000")  # Default to port 8000 if PORT is not set
-    command = f"gunicorn -b 0.0.0.0:{port} main:app"
-    os.system(command)
-    
+    app.run(debug=True, host="0.0.0.0", port=8000)
--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import re 
 
-pattern = r'\.\d{3,4}p\.'
-
+# pattern = r'\.\d{3,4}p\.'
+pattern = r'S0?1E0?13'
 stri = 'aquaman.and.the.lost.kingdom.2023.20p.uhd.bluray.x265-b0mbardiers.mkv'
+# print(stri[-2:])
 
+string = 'La.casa.de.papel.S01E13.SPA-ENG.1080p.10bit.WEBRip.2CH.x265.HEVC-PSA.mkv' 
+x = re.search(pattern=pattern,string=string)
+print(x)
+if x : 
+    print(x.group())
 
-x = re.search(pattern=pattern,string=stri)
-
-# if x : 
-print(x.group())
-
-
+# if int(' 01   ') == 1 : 
+#     print('wtf')
